0ffc3455a1577e0dfac3232cab3e00397bfcc261
baigugu <2432115441@qq.com>
2022-02-23 13:55:09 +0800

    新增内容

--- include/database.php
+++ include/database.php
@@ -1,2 +1,7 @@
 <?php
 include("./config.php");
+$mysqli_conn = mysqli_connect($mysqli_host, $mysqli_user, $mysqli_password, $mysqli_name);
+if (!$mysqli_conn) {
+    exit("Connection failed:" . mysqli_connect_error());
+}
+//异常Mysql链接捕获
