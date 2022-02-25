e16f4a6017a2966fe319c205111b9a2586ae4121
baigugu <2432115441@qq.com>
2022-02-25 13:55:36 +0800

    自动下载图片

--- include/function.php
+++ include/function.php
@@ -36,17 +36,16 @@ function curl_request($url, $post = [], $cookie = '',  $returnCookie = false)
 
 
 //下载函数
-function down_file($url, $folder = "./")
+function down_file($url,$file_name, $folder = "./")
 {
   set_time_limit(24 * 60 * 60); // 设置超时时间
   $destination_folder = $folder . '/'; // 文件下载保存目录，默认为当前文件目录
   if (!is_dir($destination_folder)) { // 判断目录是否存在
     mkdirs($destination_folder); // 如果没有就建立目录
   }
-  $newfname = $destination_folder . basename($url); // 取得文件的名称
   $file = fopen($url, "rb"); // 远程下载文件，二进制模式
   if ($file) { // 如果下载成功
-    $newf = fopen($newfname, "wb"); // 远在文件文件
+    $newf = fopen($destination_folder.$file_name, "wb"); // 远在文件文件
     if ($newf) // 如果文件保存成功
       while (!feof($file)) { // 判断附件写入是否完整
         fwrite($newf, fread($file, 1024 * 8), 1024 * 8); // 没有写完就继续
--- include/update.php
+++ include/update.php
@@ -1,53 +1,65 @@
 <?php
+//设置文件
 include("./config.php");
 //数据目录路径
 include("./function.php");
-$data_path = "..//data//";
+$data_path = "../data/";
 
 //人物数据提取
 function character_list_create()
 {
     global $data_path;
     $character_table = $data_path . "character_table.json";
-    if (is_file($character_table)) {
-        unlink($character_table);
-    }
     $json_string = file_get_contents($character_table);
-
     // 用参数true把JSON字符串强制转成PHP数组 
     $data = json_decode($json_string, true);
     // 遍历原文件提取需要的参数
     foreach ($data as $key => $value) {
         if ($value["itemObtainApproach"] == "招募寻访") {
             $character_data_single["name"] = $value["name"];
-            $character_data_single["star"] = $value["rarity"];
+            $character_data_single["star"] = $value["rarity"] + 1;
             $character_data_single["class"] = $value["profession"];
-            $character_data_single["tag"] = $value["tagList"];
+            $character_data_single["imageUrl"] = imageUrl_get($value["name"]);
             $character_data[$key] = $character_data_single;
-            echo "{$key}==>" . $value["name"] . "<br>";
+            if (!is_file($data_path . 'image/character/' . $key . '.png')) {
+                down_file($character_data_single["imageUrl"], $key . '.png', $data_path . 'image/character/');
+            }
+            if ($character_data_single["star"] == 6) {
+                $character_star_list["6"] = $key;
+            } elseif ($character_data_single["star"] == 5) {
+                $character_star_list["5"] = $key;
+            } elseif ($character_data_single["star"] == 4) {
+                $character_star_list["4"] = $key;
+            } elseif ($character_data_single["star"] == 3) {
+                $character_star_list["3"] = $key;
+            } else {
+                $character_star_list["0"] = $key;
+            }
         }
     }
+    $character_star = fopen($data_path . "star_list.json", "w");
+    $character_star_list = json_encode($character_star_list, JSON_UNESCAPED_UNICODE);
+    fwrite($character_star, $character_star_list);
+    fclose($character_star);
     $character_data_list = fopen($data_path . "character_list.json", "w");
     $character_data = json_encode($character_data, JSON_UNESCAPED_UNICODE);
     fwrite($character_data_list, $character_data);
     fclose($character_data_list);
 }
 
-function imageUrl_get($url, $name)
+//爬取图片url加入character_list.json
+function imageUrl_get($name)
 {
+    $url = curl_request("https://prts.wiki/w/PRTS:%E6%96%87%E4%BB%B6%E4%B8%80%E8%A7%88/%E5%B9%B2%E5%91%98%E7%B2%BE%E8%8B%B10%E5%8D%8A%E8%BA%AB%E5%83%8F");
     $matches_str = '/\/[0-9a-z]+\/[0-9a-z]+\/\%E5\%8D\%8A\%E8\%BA\%AB\%E5\%83\%8F_' . addcslashes(urlencode($name), "%") . '\_1\.png' . '/';
     $image_url = preg_match_all($matches_str, $url, $matches);
     $image_url_all = "https://prts.wiki//images" . $matches[0][0];
     return $image_url_all;
 }
 
-//爬取图片url加入character_list.json
 
 
-
-
-//down_file("https://githubraw.baimianxiao.cn/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/character_table.json", "../data/");
-if(true){
+//down_file("https://githubraw.baimianxiao.cn/Kengxxiao/ArknightsGameData/master/zh_CN/gamedata/excel/character_table.json","character_table.json", "../data/");
+if (true) {
     character_list_create();
 }
-
