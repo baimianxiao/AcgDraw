7112ea2a5e271ce3bfabf1fffa66ac62a4cd2732
baigugu <2432115441@qq.com>
2022-02-28 13:32:18 +0800

    修改设置文件

--- include/config.php
+++ include/config.php
@@ -1,5 +1,13 @@
 <?php
-$mysqli_host="";
-$mysqli_user="";
-$mysqli_password="";
-$mysqli_name="";
+$mysqli_host="localhost";
+$mysqli_user="user_name";
+$mysqli_password="password";
+$mysqli_name="mysql_name";
+
+/*
+文件更新使用的网站
+1.https://githubraw.baimianxiao.cn（镜像站，国内可用）
+2.https://raw.githubusercontent.com（原站，国内需要vpn）
+默认使用
+*/
+$update_address="https://github.baimianxiao.cn";
