42b43ccb8c066e61c5a165b6583fa190c56cb357
baigugu <2432115441@qq.com>
2022-03-16 17:33:11 +0800

    优化细节

--- include/test.php
+++ include/test.php
@@ -7,7 +7,7 @@ include("./config.php");
 include("./function.php");
 
 echo "<pre>";
-$table_data = get_table_data("https://prts.wiki/index.php?title=%E5%8D%A1%E6%B1%A0%E4%B8%80%E8%A7%88/%E9%99%90%E6%97%B6%E5%AF%BB%E8%AE%BF&action=edit");
+$table_data = get_table_data("https://prts.wiki/index.php?title=%E5%8D%A1%E6%B1%A0%E4%B8%80%E8%A7%88/%E9%99%90%E6%97%B6%E5%AF%BB%E8%AE%BF&action=edit", 1);
 print_r(manage_table_data($table_data));
 echo "<pre>";
 //获取
@@ -35,12 +35,18 @@ function get_table_data($url, $mode = 0)
 function manage_table_data($tableData)
 {
   foreach ($tableData as $key => $value) {
-    preg_match_all("/link=([\s\S]*?)]/", $value, $match);
-    $table_data_single["id"] = $match[1][0];
+    preg_match_all("/link=[\s\S]*?\/([\s\S]*?)]/", $value, $match);
+    if ($match[1][0] == "" or $match[1][0] == null) {
+      //如果ID为空跳出循环
+      continue;
+    }
+    $tableID= $match[1][0];
     preg_match_all("/\[\[[\s\S]*?\|([\s\S]*?)\]\]/", $value, $match);
     $table_data_single["name"] = $match[1][1];
     preg_match_all("/[\s\S]*/", $value, $match);
     $characterData = $match[0][0];
+    preg_match_all("/\d*-\d*-[\s\S]*?:\d*/", $value, $match);
+    $table_data_single["time"]=$match[0];
     preg_match_all("/{{[\s\S]*?}}/", $characterData, $match);
     $characterList = $match[0];
     $table_data_single["star6_up"] =  $characterList;
@@ -64,9 +70,7 @@ function manage_table_data($tableData)
       }
       $table_data_single["star6_up"] =  $characterList;
     }
-    if ($table_data_single["id"] != "" or $table_data_single["id"] != null) {
-      $tableddata[$table_data_single["id"]] = $table_data_single;
-    }
+    $tableddata[$tableID] = $table_data_single;
   }
   return $tableddata;
 }
