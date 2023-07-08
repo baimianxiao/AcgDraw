e23c015b963c36193add1eaadd81568453956be2
gugu <2432115441@qq.com>
2023-07-08 23:20:30 +0800

    根据星级将干员分类

--- ArknightDraw/updateArk.py
+++ ArknightDraw/updateArk.py
@@ -12,13 +12,24 @@ class UpdateHandleArk(UpdateHandle):
     # 获取人物更新信息
     async def get_info(self):
         char_data_list = {}
+        simple_star_list = {
+            6: [],
+            5: [],
+            4: [],
+            3: []
+        }
+        limit_activity = {
+            "全部活动":[],
+            "linkage":{},
+            "limit_":{}
+        }
         url = "https://wiki.biligame.com/arknights/干员数据表"
         result = await self.get_url(url)
         if not result:
             return ""
         dom = etree.HTML(result, etree.HTMLParser())
         char_list = dom.xpath("//table[@id='CardSelectTr']/tbody/tr")
-        for char in trange(len(char_list), desc="爬取人物素材", unit="char"):
+        for char in trange(len(char_list), desc="处理人物素材", unit="char"):
             char = char_list[char]
             try:
                 # 获取基本信息
@@ -49,13 +60,23 @@ class UpdateHandleArk(UpdateHandle):
             }
             # print(json.dumps(char_dict, ensure_ascii=False, indent=2))
 
+            # 稀有度分类
+            if "标准寻访" in char_dict["获取途径"]:
+                if char_dict["星级"] == 6:
+                    simple_star_list[6].append(name)
+                elif char_dict["星级"] == 5:
+                    simple_star_list[5].append(name)
+                elif char_dict["星级"] == 4:
+                    simple_star_list[4].append(name)
+                elif char_dict["星级"] == 3:
+                    simple_star_list[3].append(name)
+
             char_data_list[name] = char_dict
 
         # print(json.dumps(char_data_list, ensure_ascii=False, indent=2))
-
-        with open(self.data_path + 'char_data_list.json', 'w', encoding='utf-8') as f:
-            f.write(json.dumps(char_data_list, ensure_ascii=False, indent=2))
-            return char_data_list
+        json_write(self.data_path + 'simple_star_list.json',simple_star_list)
+        json_write(self.data_path + 'char_data_list.json', char_data_list)
+        return char_data_list
 
     # 下载图片数据
     async def char_image_download(self, char_list):
@@ -65,14 +86,33 @@ class UpdateHandleArk(UpdateHandle):
                 name = list(char_list)[char]
                 await self.download_file(char_list[name]["半身像"], "半身像_" + name + ".png", download_path)
                 await self.download_file(char_list[name]["立绘"], "立绘_" + name + ".png", download_path)
-                pbar.set_postfix(prograss=str(name)+"下载完毕")
+                pbar.set_postfix(prograss=str(name) + "下载完毕")
                 pbar.update(1)
 
     def start_update(self):
-        loop = asyncio.get_event_loop()
 
+        loop = asyncio.get_event_loop()
         char_list = loop.run_until_complete(self.get_info())
+
         loop.run_until_complete(self.char_image_download(char_list))
+
+
+def json_write(path, data) -> bool:
+    try:
+        with open(path, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(data, ensure_ascii=False, indent=2))
+        return True
+    except:
+        return False
+
+
+def json_read(path):
+    try:
+        with open(path, 'r', encoding='utf-8') as f:
+            data = f.read()
+        return json.loads(data)
+    except:
+        return False
 
 
 if __name__ == "__main__":
