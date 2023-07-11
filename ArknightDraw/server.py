ef062ae720954c2b4c0dda8aa1fd3efe02a9b447
gugu <2432115441@qq.com>
2023-07-11 19:47:14 +0800

    接入十连抽

--- ArknightDraw/server.py
+++ ArknightDraw/server.py
@@ -1,7 +1,9 @@
 # -*- encoding:utf-8 -*-
+import os
 
+import drawHandleArk
 from flask import Flask, send_file
-from PIL import Image
+
 # import numpy as np
 import io
 
@@ -10,18 +12,17 @@ app = Flask(__name__)
 
 @app.route("/arknights/arknightsdraw", methods=['POST', 'GET'])
 def arknights():
-    return "sss"
+    img = drawHandleArk.ten_draw()
+    file_object = io.BytesIO()
+    img.save(file_object, 'PNG')
+    file_object.seek(0)
+    return send_file(file_object, mimetype='image/PNG')
 
 
 @app.route('/', methods=['POST', 'GET'])
 def arknights_draw():
-    # 从数组加载图片
-    # arr = np.array(raw_data)
-    # img = Image.fromarray(arr.astype('uint8'))
-    img = Image.open("../docs/main.png")
-    # 在内存中创建图片对象
+    img = drawHandleArk.ten_draw()
     file_object = io.BytesIO()
-    # write PNG in file-object
     img.save(file_object, 'PNG')
     file_object.seek(0)
     return send_file(file_object, mimetype='image/PNG')
