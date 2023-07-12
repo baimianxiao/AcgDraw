70e3b51fbe4e17b5e31e87c7501bf19e275d3a04
gugu <2432115441@qq.com>
2023-07-12 17:49:43 +0800

    命令行添加手动更新选项

--- main.py
+++ main.py
@@ -22,14 +22,29 @@ def mkdir(path):
 create_dir_list = [
     "./static/image",
     "./templates",
-    "./data/"
+    "./data/",
+    "./conf"
 ]
-for create_dir in create_dir_list:
-    mkdir(create_dir)
 
+print("Arknights-Draw")
+input("按任意键继续")
 if not os.path.exists("./data/lock.lock"):
-    ArknightDraw.updateArk.UpdateHandleArk("./data/Arknights/","./conf/Arknights/").start_update()
-    with open("./data/lock.lock", 'w', encoding='utf-8') as f:
-        f.write("")
-ArknightDraw.server.server_start()
-
+    print("服务器未部署，开始部署")
+    for create_dir in create_dir_list:
+        mkdir(create_dir)
+    try:
+        ArknightDraw.updateArk.UpdateHandleArk("./data/Arknights/", "./conf/Arknights/").start_update()
+        with open("./data/lock.lock", 'w', encoding='utf-8') as f:
+            f.write("")
+        ArknightDraw.server.server_start()
+    except:
+        print("部署发生错误，请重试")
+        input("按任意键继续")
+else:
+    print("服务器已部署\n1.启动服务器\n2.启动更新")
+    x = input("请选择操作:")
+    if int(x) == 1:
+        ArknightDraw.server.server_start()
+    elif int(x) == 2:
+        ArknightDraw.updateArk.UpdateHandleArk("./data/Arknights/", "./conf/Arknights/").start_update()
+        ArknightDraw.server.server_start()
